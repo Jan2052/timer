58d2114f466494e05752f021a3a84abdf2fd1b5d
jan2052 <janet_lam@live.ca>
2022-08-21 22:52:21 -0400

    refactored removed function sound() and made it one function

--- timer1.js
+++ timer1.js
@@ -1,15 +1,16 @@
 let args = process.argv.slice(2);
-console.log(args)
+// console.log(args)
 
 let argsArr = args.map(x => Number(x)).filter(x => x > 0 && x !== "" && typeof x === "number")
-console.log(argsArr)
+// console.log(argsArr)
 
 function boop() {
   for (let i = 0; i < argsArr.length; i++) {
-      setTimeout(() => {
-        process.stdout.write('\x07');
-        console.log("Time", args[i])
-      }, argsArr[i] * 1000);
+
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      // console.log("Time", args[i])
+    }, argsArr[i] * 1000);
   }
 }
 
