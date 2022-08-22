323ba78a2cb4058fe0da30b0a4ba5ef8e3e1460d
jan2052 <janet_lam@live.ca>
2022-08-21 23:37:01 -0400

    removed some console.logs line 2,4

--- timer1.js
+++ timer1.js
@@ -1,8 +1,6 @@
 let args = process.argv.slice(2);
-// console.log(args)
 
 let argsArr = args.map(x => Number(x)).filter(x => x > 0 && x !== "" && typeof x === "number")
-// console.log(argsArr)
 
 function boop() {
   for (let i = 0; i < argsArr.length; i++) {
@@ -15,5 +13,3 @@ function boop() {
 }
 
 boop()
-
-//Peer coding with Catherine Wu
