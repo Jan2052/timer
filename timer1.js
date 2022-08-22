cb22a4a80befeb4379ca46f4a8e8209200514017
jan2052 <janet_lam@live.ca>
2022-08-21 22:45:31 -0400

    removed "function sound"

--- timer1.js
+++ timer1.js
@@ -4,17 +4,13 @@ console.log(args)
 let argsArr = args.map(x => Number(x)).filter(x => x > 0 && x !== "" && typeof x === "number")
 console.log(argsArr)
 
-function loop() {
+function boop() {
   for (let i = 0; i < argsArr.length; i++) {
-    beep()
-
-    function beep() {
       setTimeout(() => {
         process.stdout.write('\x07');
         console.log("Time", args[i])
       }, argsArr[i] * 1000);
-    }
   }
 }
 
-loop()
+boop()
